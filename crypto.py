d36d5aa9343b592f74cf9e78fa9f5206380fff5f
Coco Layton <cocolayton@Cocos-MacBook-Pro.local>
2020-09-28 01:37:11 -0700

    fixed bug

--- crypto.py
+++ crypto.py
@@ -245,24 +245,53 @@ def encrypt_mhkc(plaintext, public_key):
 
     return encryption
 
+def modInverse(a, m) : 
+    m0 = m 
+    y = 0
+    x = 1
+  
+    if (m == 1) : 
+        return 0
+  
+    while (a > 1) : 
+  
+        # q is quotient 
+        q = a // m 
+        t = m 
+  
+        # m is remainder now, process same as Euclid's algo 
+        m = a % m 
+        a = t 
+        t = y 
+  
+        # Update x and y 
+        y = x - q * y 
+        x = t
+  
+    # Make x positive 
+    if (x < 0) : 
+        x = x + m0 
+  
+    return x
+
 
 def get_C_prime(R, Q, ciphertext, n = 8):
-    S = (1/R)%Q
-    c_prime = ciphertext*S%Q
+    r_prime = modInverse(R, Q)
+    c_prime = ciphertext * r_prime % Q
     return c_prime
 
-def get_W_values(c_prime, W):
+def get_W_values(c_prime, W, n = 8):
     indices_list = []
     target = c_prime
 
-    for index in range(n):
+    for index in (reversed(range(n))):
         if W[index] <= target:
-            indices_list.append(index)
+            indices_list.append(index+1) # +1 because W goes from 1-8 not 0-7 so need to get correct index
+            target = target - W[index]
 
-        target = target - W[index]
-
-        if target <= 0:
-            return W_sum
+        if target == 0:
+            #print("list", indices_list)
+            return indices_list
 
 # Arguments: list of integers, private key (W, Q, R) with W a tuple
 # Returns: bytearray or str of plaintext
@@ -275,15 +304,20 @@ def decrypt_mhkc(ciphertext, private_key, n = 8):
 
     for value in ciphertext:
         c_prime = get_C_prime(R, Q, value)
-        indicies_list = get_W_values(c_prime, W)
+        indices_list = get_W_values(c_prime, W)
+
         ascii_num = 0
         
-        for index in indicies_list:
-            ascii_num = ascii_num + 2**(n-index)
+        for index in indices_list:
+            ascii_num = ascii_num + (2**(n-index))
 
         decryption_bytes.append(ascii_num)
 
-    return decryption_bytes
+    message = ""
+    for value in decryption_bytes:
+        message = message + chr(value)
+
+    return message
 
 
 def main():
@@ -302,13 +336,14 @@ def main():
 
     #testing last one
 
-    plaintext = ""
-    public_key = (10, 20, 56, 118, 288, 618, 1376, 4826)
-    private_key = ((5, 10, 28, 59, 144, 309, 688, 2413), 5575, 2)
+    plaintext = "POTATOSALAD"
+    public_key = (18, 28, 54, 136, 254, 740, 1714, 3194)
+    private_key = ((9, 14, 27, 68, 127, 370, 857, 1597), 4745, 2)
     encrypt = encrypt_mhkc(plaintext, public_key)
-    decrypt = (encrypt, private_key)
+    decrypt = decrypt_mhkc(encrypt, private_key)
 
-    print(encrypt)
+    #print(encrypt)
+    print(decrypt)
 
     
 
